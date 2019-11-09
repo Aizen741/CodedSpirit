15c4a2ff647fb0a2fca038b80b3c9b9c64cc9bd8
Aizen741 <rahulsivadas2009@gmail.com>
2019-11-09 22:07:35 +0530

    CategoryConstructorAdded

---
+++ app/src/main/java/com/example/codedspirit/Adapter/CategoryRecycleAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.codedspirit.Adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CategoryRecycleAdapter : RecyclerView.Adapter<CategoryRecycleAdapter.holder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): holder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: holder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    inner class holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+}

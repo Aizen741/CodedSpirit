217f40cb2bec2aa4f5a553b08a23a9958acf64ce
Aizen741 <rahulsivadas2009@gmail.com>
2019-11-09 10:05:58 +0530

    CategoryAdapterAdded

--- app/src/main/java/com/example/codedspirit/Controller/MainActivity.kt
+++ app/src/main/java/com/example/codedspirit/Controller/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.codedspirit.Controller
 
-import android.content.AbstractThreadedSyncAdapter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import com.example.codedspirit.Model.Category
+import androidx.appcompat.app.AppCompatActivity
+import com.example.codedspirit.Adapter.CategoryAdapter
 import com.example.codedspirit.R
 import com.example.codedspirit.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,14 +12,11 @@ class MainActivity : AppCompatActivity() {
     // Adapter is used to send the data stored in the data service to  the list view, which manages it and show it on the UI
 
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,DataService.categories)
-
+        adapter = CategoryAdapter(this,DataService.categories)
         categoryListView.adapter = adapter
 
     }

69f054c4c98acb53af65cecc1d9ac21609362dd6
Aizen741 <rahulsivadas2009@gmail.com>
2019-11-10 23:07:20 +0530

    ProductAdapter

---
+++ app/src/main/java/com/example/codedspirit/Adapter/ProductsAdapters.kt
@@ -0,0 +1,24 @@
+package com.example.codedspirit.Adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ProductsAdapters:RecyclerView.Adapter<ProductsAdapters.ProductHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ProductHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    inner class ProductHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+
+}

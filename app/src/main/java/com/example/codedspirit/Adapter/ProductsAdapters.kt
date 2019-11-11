6cfe1665b9f55b31967f5330c7a9fe546de460de
Aizen741 <rahulsivadas2009@gmail.com>
2019-11-11 10:10:37 +0530

    ProductAdapterEdited

--- app/src/main/java/com/example/codedspirit/Adapter/ProductsAdapters.kt
+++ app/src/main/java/com/example/codedspirit/Adapter/ProductsAdapters.kt
@@ -1,24 +1,47 @@
 package com.example.codedspirit.Adapter
 
+import android.content.Context
+import android.view.ContextMenu
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.codedspirit.Model.Product
+import com.example.codedspirit.R
+import kotlinx.android.synthetic.main.product_list_item.view.*
 
-class ProductsAdapters:RecyclerView.Adapter<ProductsAdapters.ProductHolder>() {
+class ProductsAdapters(val context: Context, val products: List<Product>):RecyclerView.Adapter<ProductsAdapters.ProductHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(context).inflate(R.layout.product_list_item,parent,false)
+        return ProductHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return products.count()
     }
 
     override fun onBindViewHolder(holder: ProductHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder?.bindProduct(products[position],context)
     }
 
 
-    inner class ProductHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class ProductHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val productImage = itemView?.findViewById<ImageView>(R.id.productimage)
+        val productName = itemView?.findViewById<TextView>(R.id.categoryName)
+        val productPrice = itemView?.findViewById<TextView>(R.id.productPrice)
+
+
+        fun bindProduct(product: Product,context: Context){
+            val resourceId = context.resources.getIdentifier(product.image,"drawable",context.packageName)
+            productImage?.setImageResource(resourceId)
+            productName?.text = product.title
+            productPrice?.text = product.price
+        }
+
+    }
 
 
 }
